936e245713ae307d2dd992ab0b7171a256f43e90
Tanjina Akter Nisa <140683300+tanjinanisa86@users.noreply.github.com>
2024-01-24 09:49:07 +0600

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Lab5Pc-24
+ */
+public class EvenNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        for(i=2;i<25;i=i+2){
+            System.out.println(i);
+        }
+        // TODO code application logic here
+    }
+    
+}
